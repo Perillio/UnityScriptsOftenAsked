41713248703fc79dfff0419d5c36c94d92e85477
Perillio <47478204+Perillio@users.noreply.github.com>
2020-07-08 15:52:02 +0200

    Create MoveToClick.cs
    Move an object by clicking. This needs a baked navmesh to work. For Playermovement this works like in the game Diablo.

---
+++ MoveToClick.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+//Moves object to clicked position (diablo like movement for player). Needs a baked navmesh !
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveToClick : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
