03fbcd7f9f51bf7292af6b7c4d98e37a9e3a51ae
Perillio <47478204+Perillio@users.noreply.github.com>
2021-06-29 20:35:48 +0200

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    //-------------------------------- ( SETUP YOUR CODE HERE : ) ------------------------------------------//
+    public void DoAction()
+    {
+        Debug.Log("ACTION!");
+        //Action code here ...
+    }
+
+    public void DoJump()
+    {
+        Debug.Log("Boing!");
+        //Jump code here...
+    }
+
+
+    //-------------------------------- ( DO NOT CHANGE AFTER HERE : ) -------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    [Header("Other Controls:")]
+    public KeyCode p_forward = KeyCode.W;
+    public KeyCode p_backward = KeyCode.S;
+    public KeyCode p_strafeLeft = KeyCode.A;
+    public KeyCode p_strafeRight = KeyCode.D;
+
+    public KeyCode p_up = KeyCode.Space;
+    public KeyCode p_down = KeyCode.X;
+    public KeyCode p_action = KeyCode.F;
+    public bool isJumpInsteadUp = false;
+    [TextArea]
+    public string important = "Rotation is handled by Mouse. Up and Down sight has to be done in camera ! (Also Zooming etc)";
+
+    [Header("Player Settings:")]
+    public float speed = 10f;
+    public float mouseSensitivity = 25f;
+
+    [Header("DEBUG:")]
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 movement;
+    public Vector3 viewpoint;
+    public float viewDegrees;
+    public int currentFPS;
+    public int highestFPS;
+    public int lowestFPS = 99999;
+    public int averageFPS;
+
+    private float frameTimer = 1f;
+    private int frameCounter = 0;
+    private int totalCounts = 0;
+    private int totalFrames = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    private void Awake()
+    {
+        lowestFPS = 99999;
+    }
+    void Update()
+    {
+        float h = 0;
+        if (Input.GetKey(p_strafeLeft) && !Input.GetKey(p_strafeRight)) { h = -1; }
+        if (!Input.GetKey(p_strafeLeft) && Input.GetKey(p_strafeRight)) { h = 1; }
+        float v = 0;
+        if (Input.GetKey(p_backward) && !Input.GetKey(p_forward)) { v = -1; }
+        if (!Input.GetKey(p_backward) && Input.GetKey(p_forward)) { v = 1; }
+        float ud = 0f;
+        if (Input.GetKey(p_up) && !Input.GetKey(p_down)) { ud = 1f; }
+        if (!Input.GetKey(p_up) && Input.GetKey(p_down)) { ud = -1f; }
+        float mx = Input.GetAxis("Mouse X");
+        if (Input.GetKeyDown(p_action)) { DoAction(); }
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+        transform.position += right * (h * Time.deltaTime * speed);
+        transform.position += forward * (v * Time.deltaTime * speed);
+        transform.Rotate(0f,(Time.deltaTime * mx * mouseSensitivity), 0f);
+        if (isJumpInsteadUp)
+        {
+            if (Input.GetKeyDown(p_up))
+            {
+                DoJump();
+            }
+        }
+        else
+        {
+            Vector3 trup = transform.up;
+            transform.position += trup * (ud * Time.deltaTime * speed);
+        }
+        movement = new Vector3(h, ud, v);
+        position = transform.position;
+        rotation = transform.rotation.eulerAngles;
+        viewpoint = transform.forward;
+        float vd = transform.rotation.eulerAngles.y * 100;
+        vd = (int)vd;
+        viewDegrees = vd / 100;
+        frameCounter++;
+        frameTimer -= Time.deltaTime;
+        if (frameTimer <= 0f)
+        {
+            currentFPS = frameCounter;
+            if (currentFPS > highestFPS) { highestFPS = currentFPS; }
+            if (currentFPS < lowestFPS && currentFPS != 0 && totalCounts > 5) { lowestFPS = currentFPS; }
+            frameCounter = 0;
+            frameTimer = 1f;
+            if (totalCounts < 25000)
+            {
+                totalCounts++;
+                totalFrames += currentFPS;
+                averageFPS = totalFrames / totalCounts;
+            }
+        }
+    }
+}
